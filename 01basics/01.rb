889695ea98675a0fe83cabc3cabbcfa1f30b1fd3
EFFY MIN <effyverse@gmail.com>
2020-08-23 10:50:42 -0400

    add exercises 01 and 02

---
+++ 01basics/01.rb
@@ -0,0 +1,5 @@
+first = "Ef" 
+last = "Min"
+full_name = "#{first}" + " #{last}"
+
+test
---
+++ 01basics/02.rb
@@ -0,0 +1,7 @@
+# Exercise 2. Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the: 1) thousands place 2) hundreds place 3) tens place
+
+n = 1234
+p thousands = n/1000
+p hundreds = n%1000/100
+p tens = n%100/10
+p ones = n%10
