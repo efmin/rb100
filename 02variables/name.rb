1b2c79577156573ebcf9c4082fb20ddd7c728093
EFFY MIN <effyverse@gmail.com>
2020-08-24 15:51:46 -0400

    exercise 4 and 5 on name.rb

--- 02variables/name.rb
+++ 02variables/name.rb
@@ -1,7 +1,19 @@
-
-
+# exercise 1
 
 puts "What is your name?"
 name = gets.chomp
 puts "Is your name really " + name + "?"
 
+# exercise 3
+
+10.times do 
+  puts name
+end
+
+# exercise 4 
+
+puts "What is your first name?"
+first = gets.chomp
+puts "what is your last name?"
+last = gets.chomp 
+puts "Hmmm, is your full name really " + first + " " + last + "?"
