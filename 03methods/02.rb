f43482e0245d20c704ae2300c28627d4e3c4c2f3
EFFY MIN <effyverse@gmail.com>
2020-08-25 10:07:35 -0400

    exercises 2-4

---
+++ 03methods/02.rb
@@ -0,0 +1,14 @@
+1. 2
+var x is assigned to value of 2. Return value is 2.
+
+2. nil 
+puts has no return value
+
+3. "Joe" 
+p has return value but puts and print do not
+
+4. "four"
+var four is assigned the string value of "four" which is the return value
+
+5. nil
+print (similar to puts) has no return value
