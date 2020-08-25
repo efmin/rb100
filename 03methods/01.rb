d8c7eb6d372bde7136d230ba5d4f19bcfcd9f6f2
EFFY MIN <effyverse@gmail.com>
2020-08-25 09:41:02 -0400

    exercise 01

---
+++ 03methods/01.rb
@@ -0,0 +1,15 @@
+# def greeting(name)
+  # greeting = "Hello " + name + "!"
+  # puts greeting
+ # greeting
+# end
+
+#greeting("Poop")
+
+ # Refactored after viewing LS solution:
+
+def greeting(name)
+  "Hello " + name + "!"
+end
+
+puts greeting("Shortie")
