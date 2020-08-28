342ef7452fa9cbda2370be8fc8531e6ff38c6f37
EFFY MIN <effyverse@gmail.com>
2020-08-28 09:17:08 -0400

    first exercise

---
+++ 06_arrays.rb/01.rb
@@ -0,0 +1,10 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(number)
+  puts "The number #{number} is included in the array."
+end
+
+
+# Note: 
+# When I try to put this program into a method but then I call the method on the array, it says that I cannot use a private method. Encapsulation? Think on more later. Try to write programs without putting them into methods for now. 
