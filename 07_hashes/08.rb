d43db492e534e61d49a005c93c8acfa3915f5a82
EFFY MIN <effyverse@gmail.com>
2020-08-29 12:08:46 -0400

    write out challenge algorithmn question using IODINE TEST

--- 07_hashes/08.rb
+++ 07_hashes/08.rb
@@ -1,6 +1,40 @@
 # Write a program that prints out groups of words that are anagrams. Anagrams are words that have the same exact letters in them but in a different order. Your output should look something like this:
+#
+# ["demo", "dome", "mode"]
+# ["neon", "none"]
 
 
 words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
+
+
+# IODINE TEST
+#
+# Input:
+# - array of 4 letter words already supplied
+#
+# Output:
+# - array of anagrams
+#
+# Definitions:
+# - Anagrams: words with exact same letters but different order
+#
+# Illustrations:
+# - n/a 
+#
+# New/Methods:
+# - sort 
+#
+# Exceptions/Examples:
+# - array is already supplied so we are protected from edge cases! ;)
+#
+#
+# TEST OFTEN!!
+#
+#
+#
+#
+#
+#
+#
