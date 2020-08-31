8e46a99f0548a04f036d64a79df85a3713094c30
EFFY MIN <effyverse@gmail.com>
2020-08-31 08:05:43 -0400

    realized that the question requires a much simpler response at this level of prep

--- exercises_from_ls/11.rb
+++ exercises_from_ls/11.rb
@@ -1,36 +1,6 @@
-contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
-            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
-
-contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
-
-# IODINE TEST
-#
-# Input
-# - an array of data 
-#
-#
-# Output
-# - a hash
-#
-# Definitions
-# N/A
-#
-# Illustrations
-# - I need a method that searches the contact data array for a name that matches the contacts hash. Then I need to input the data in the matching array into the hash.
-#
-#
-# New/Methods
-#
-# Examples/Exceptions
-# - I guess this isn't how a real world app would work? 
-#
-# TEST OFTEN!
-#
-# PSEUDO CODE:
-#
-# - iterate through hash collection and save each first name into a variable that can be used to match
-# - iterate through array layer 1  to match
-# - if there is a match, take that array and save it into the hash value for the name
-#
-#
-#
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
