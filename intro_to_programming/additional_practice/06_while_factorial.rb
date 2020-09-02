f27b56623643287adb5bcc723c6e5b98ad111b35
EFFY MIN <effyverse@gmail.com>
2020-09-02 09:46:11 -0400

    refactor recursion factorial to use ternary operator

--- intro_to_programming/additional_practice/06_while_factorial.rb
+++ intro_to_programming/additional_practice/06_while_factorial.rb
@@ -1,9 +1,12 @@
 # factorial method using while
 #
 def factorial(n)
-
-
-
+  result = n
+  while n > 1
+    result = result * (n-1)
+    n -= 1
+  end
+  result
 end
 
-p factorial(n)
+p factorial(5)
---
+++ intro_to_programming/additional_practice/06_while_factorial.rb~
@@ -0,0 +1,9 @@
+# factorial method using while
+#
+def factorial(n)
+
+
+
+end
+
+p factorial(n)
