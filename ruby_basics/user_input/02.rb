86824ceb075e8c38e986a1e1466964cc8961dba0
EFFY MIN <effyverse@gmail.com>
2020-09-04 08:50:51 -0400

    user input 1-2 exercises

---
+++ ruby_basics/user_input/02.rb
@@ -0,0 +1,5 @@
+puts "What is your age in years?"
+years = gets.chomp.to_i
+months = years * 12
+
+puts "You are #{months} months year old. "
---
+++ ruby_basics/user_input/03.rb
@@ -0,0 +1,4 @@
+puts "Do you want to print something? y/n"
+ans = gets.chomp
+
+# write as a one-liner
