ba39a8d45c7ab49c81f341781a271f41065d9530
EFFY MIN <effyverse@gmail.com>
2020-10-10 09:39:37 -0400

    exercise 4

---
+++ ruby_basics/loops_1/redo_exercises/03.rb
@@ -0,0 +1,11 @@
+iterations = 1 
+
+loop do 
+  puts "Number of iterations = #{iterations}"
+  iterations = iterations + 1
+  if iterations == 6
+    break
+  end
+end
+
+
---
+++ ruby_basics/loops_1/redo_exercises/04.rb
@@ -0,0 +1,7 @@
+loop do
+  puts "Should I stop looping?"
+  answer = gets.chomp.downcase!
+  break if answer == "yes"
+end
+
+
