56c42a1b8fd1fe4fa713a99d1b550905d548c388
EFFY MIN <effyverse@gmail.com>
2020-10-14 12:35:51 -0400

    exercises from loop_2

--- ruby_basics/loops_2/redo_exercises/03.rb
+++ ruby_basics/loops_2/redo_exercises/03.rb
@@ -1,9 +1,11 @@
 process_the_loop = [true, false].sample
 
-# if process_the_loop == true
-  # puts "The loop was processed!"
-# else
-  # puts "The loop wasn't processed!"
-# end
+if process_the_loop == true
+  loop do
+    puts "The loop was processed!"
+    break
+  end
+else
+ puts "The loop wasn't processed!"
+end
 
-puts process_the_loop == true ? "processed!" : "not processed!"
