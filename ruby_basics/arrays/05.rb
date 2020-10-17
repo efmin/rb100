8540a38898c2848b62d8f03d2b5455aa4f2967f5
EFFY MIN <effyverse@gmail.com>
2020-10-17 10:23:41 -0400

    arrays exercises 5-10

---
+++ ruby_basics/arrays/05.rb
@@ -0,0 +1,2 @@
+colors = %w(red yellow purple green)
+colors.each {|c| puts "I am the color #{c}!" }
---
+++ ruby_basics/arrays/08-09.rb
@@ -0,0 +1,5 @@
+# exercises 8-9 
+
+favourites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+ p favourites.flatten!
+ 
