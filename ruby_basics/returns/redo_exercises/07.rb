f612cf46411ecd07f7a29aedcef636a35d2f17c8
EFFY MIN <effyverse@gmail.com>
2020-10-20 12:45:52 -0400

    redo exercises 7-10 of returns

---
+++ ruby_basics/returns/redo_exercises/07.rb
@@ -0,0 +1,9 @@
+def count_sheep
+  5.times do |sheep|
+    puts "sheep"
+  end
+end
+
+puts "should print sheep 5 times on separate lines and then the integer 5"
+puts count_sheep
+
---
+++ ruby_basics/returns/redo_exercises/08.rb
@@ -0,0 +1,9 @@
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+  end
+  10
+end
+
+puts count_sheep
+puts "this should print 0, 1, 2, 3, 4 and 10"
